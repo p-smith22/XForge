87f279437fe3dc69c79534ab82aaf4596c03433c
andrewkedash-abk <andrewkedash@gmail.com>
2026-05-03 14:39:14 -0400

    Refactor Plane class with constructors and new methods

--- XForge/Plane.h
+++ XForge/Plane.h
@@ -1,32 +1,55 @@
 #pragma once
 #include "XFoil_Interface.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Plane: public XFoil_Interface {
-	private:
-		// set chord, wingspan, epsilon (oswald efficiency factor)
-		double setChord(double Chord);
-		double setWingspan(double Wingspan);
-		double setEpsilon(double Epsilon);
+class Plane : public XFoil_Interface {
+public:
+    // Constructors and overidden constructors
+    Plane();
+    Plane(double chord, double wingspan, double epsilon, double rho, double Vinf);
 
-		// get chord, wingspan, epsilon
-		void getChord();
-		void getWingspan();
-		void getEpsilon();
+    // setters
+    double setChord(double Chord);
+    double setWingspan(double Wingspan);
+    double setEpsilon(double Epsilon);
+    void   setRho(double Rho);
+    void   setVinf(double vinf);
 
-		// calculate the aspect ratio
-		double calculateAspectRatio(double chord, double wingspan);
-		// calculate the 3D lift, 3D lift slope, 3D drag
-		double calculateLift();
-		// Calculate the 2D lift to drag ratio, 3D lift to drag ratio
-	public:
-		double wingspan;
-		double chord;
-		double aspectRatio;
-		double epsilon;
-		double lift3D;
-		double liftSlope3D;
-		double drag3D;
+    // Getters
+    void getChord()   const;
+    void getWingspan() const;
+    void getEpsilon() const;
 
+    // Geometry:
+    double calculateAspectRatio();
+
+    // Aerodynamic calculations (2D to 3D)
+    double calculate3DLiftSlope();          // a3D  (1/rad)
+    double calculate3DAoA(double alpha2D, double CL2D);
+    double calculatedrag3D(double CD2D, double CL3D);
+
+    // Lift-to-drag ratios at alpha:
+    pair<double, double> calculateLift2Drag(int alphaIdx); // returns 2D and 3D
+
+    // Schrenk approximation
+    vector<double> schrenkApproximation();
+
+    // Prandtl Lifting Line Theory
+    vector<double> liftingLineTheory(int N = 30); // returns sectional lift (N/m) for plotting
+
+protected:
+    vector<DigestPolar> polar;
+
+private:
+    double wingspan;
+    double chord;
+    double aspectRatio;
+    double epsilon;      // Oswald eff. factor
+    double rho;          // air density (kg/m^3)
+    double Vinf;         // freestream velocity (m/s)
+    double liftSlope3D;
+    double lift3D;
+    double drag3D;
 };
